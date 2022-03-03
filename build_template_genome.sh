aa187d2836360f545f40fc85463615970f990001
red-t <2860257556@qq.com>
2022-03-03 15:23:25 +0800

    update all

--- build_template_genome.sh
+++ build_template_genome.sh
@@ -45,7 +45,7 @@ echo -e "M_CHROMS:\t${M_CHROMS[*]}"
 if [ ! -f ${NAME}.tmp.F.${CHROM}.snp.bcf ];then
     mkdir ${NAME}_templateswithsnp
 
-    if [ ${SEX}=="F" ];then
+    if test ${SEX} = "F";then
         for CHROM in ${F_CHROMS[*]}
         do
             VCF=`readlink -f ${VCF_PATH%/}/*${CHROM}.*vcf.gz`
@@ -59,7 +59,7 @@ if [ ! -f ${NAME}.tmp.F.${CHROM}.snp.bcf ];then
         mv ${NAME}.tmp.F* ${NAME}_templateswithsnp
     fi
 
-    if [ ${SEX}=="M" ];then
+    if test ${SEX} = "M";then
         mkdir haplo2
         for CHROM in ${F_CHROMS[*]}
         do
@@ -67,7 +67,7 @@ if [ ! -f ${NAME}.tmp.F.${CHROM}.snp.bcf ];then
             bcftools view --threads 10 -v snps -O b -o ${NAME}.tmp.M.${CHROM}.snp.bcf -s ${NAME} -m2 -M2 -c1 -C1 ${VCF}
             bcftools index ${NAME}.tmp.M.${CHROM}.snp.bcf
             bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%SAMPLE=%GT]\n' ${NAME}.tmp.M.${CHROM}.snp.bcf | grep "1|0" | awk 'OFS=FS="\t"''{print $1, ($2 -1), $2, "SNP", $4, "0"}' >> ${NAME}.tmp.M.snp.h1.bed
-            if [ ${CHROM}!="chrX" ];then
+            if test ${CHROM}!="chrX";then
                 bcftools query -f '%CHROM\t%POS\t%REF\t%ALT[\t%SAMPLE=%GT]\n' ${NAME}.tmp.M.${CHROM}.snp.bcf | grep "0|1" | awk 'OFS=FS="\t"''{print $1, ($2 -1), $2, "SNP", $4, "0"}' >> ${NAME}.tmp.M.snp.h2.bed
             fi
         done
