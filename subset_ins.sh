b78c2ec26c8868b7e744719805ee276d26bdfb31
red-t <2860257556@qq.com>
2022-03-08 19:13:40 +0800

    update all

--- subset_ins.sh
+++ subset_ins.sh
@@ -27,23 +27,25 @@ while getopts ":n:M:R:s:S:h" OPTION; do
     esac
 done
 
+export PATH=$PATH:/data/tusers/zhongrenhu/Software/anaconda3/bin/
 
-### checking ###
+
+### Checking ###
 echo -e "NAME:\t${NAME}"
 echo -e "N_INS:\t${N_INS}"
 echo -e "REF_SUMMARY:\t${REF_SUMMARY}"
 echo -e "SEX:\t${SEX}"
 echo -e "G_SIZE:\t${G_SIZE}"
-### checking ###
+### CHEKING ###
 
 
 ### GENERATE A GROUP OF SUBSET(S) AND CALCULATE INSERTION FREQUENCY FOR EACH GROUP ###
 if [ -z ${SEX} ];then
     echo -e "[ GENERATE SUBSET(S) FOR ${NAME} START. ]"
 
-    # GENERATE SUBSET(S)
     for ((j=1; j<=${G_SIZE}; j++))
     do
+        # GENERATE SUBSET(S)
         if [ ! -f ${NAME}.homozygous.groundtruth.summary ];then
             echo -e "[ CMD:\tpython /data/tusers/zhongrenhu/for_SMS/test/my_shuf.py -p ${NAME}.${j} -M ${N_INS} -R ${REF_SUMMARY} -H ]"
             python /data/tusers/zhongrenhu/for_SMS/test/my_shuf.py -p ${NAME}.${j} -M ${N_INS} -R ${REF_SUMMARY} -H
@@ -55,10 +57,19 @@ if [ -z ${SEX} ];then
             python /data/tusers/zhongrenhu/for_SMS/test/my_shuf.py -p ${NAME}.${j} -M ${N_INS} -R ${REF_SUMMARY}
             sort -k1,1 -k2,2n ${NAME}.${j}.groundtruth.summary > tmp.gt && mv tmp.gt ${NAME}.${j}.groundtruth.summary
         fi
-        
+
         cut -f 1-6 ${NAME}.${j}.groundtruth.summary > ${NAME}.${j}.groundtruth.bed
         cat ${NAME}.homozygous.groundtruth.summary >> tmp.all
         cat ${NAME}.${j}.groundtruth.summary >> tmp.all
+
+        # INTEGRATE INSERTIONS INTO TEMPLATE GENOME
+        if [ ! -f ${NAME}.${j}.fa.fai ];then
+            echo -e "[ INTEGRATE ${NAME}.homozygous.groundtruth.bed AND ${NAME}.${j}.groundtruth.bed INTO ${NAME}_template.fa START. ]"
+            mkdir tmp && cat ${NAME}.homozygous.groundtruth.bed ${NAME}.${j}.groundtruth.bed > tmp.bed
+            VISOR HACk -g ${NAME}_template.fa -b tmp.bed -o tmp
+            mv tmp/h1.fa ./${NAME}.${j}.fa && mv tmp/h1.fa.fai ./${NAME}.${j}.fa.fai && rm -r tmp && rm tmp.bed
+            echo -e "[ INTEGRATION FOR ${NAME}.${j} FINISH. ]"
+        fi
     done
 
     # CALCULATING FREQUENCY
@@ -68,6 +79,5 @@ if [ -z ${SEX} ];then
     echo -e "name\tte_seq\ttsd_seq\tins_seq\torigin_seq" > ./${NAME}.groundtruth.summary.seq
     awk 'BEGIN{OFS="\t"} {print $8,$13,$16,$6,$18}' ./tmp.uniq >> ./${NAME}.groundtruth.summary.seq
     rm tmp.uniq && rm tmp.all
-
     echo -e "[ GENERATE SUBSET(S) FOR ${NAME} FINISH. ]"
 fi
