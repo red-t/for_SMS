a9d70b8c0a3675c479e9ae39b0e77b57112a6d7c
zred-t <2860257556@qq.com>
2024-04-11 08:30:29 -0400

    feat: enable auto-tuning minEdge for assembling

--- TEMP3/Assemble.pyx
+++ TEMP3/Assemble.pyx
@@ -5,8 +5,24 @@ from subprocess import Popen, DEVNULL
 ######################
 ### Local Assembly ###
 ######################
+cdef int getMinEdge(int numSegRaw):
+    int minEdge = 3
+
+    if numSegRaw < 5:
+        minEdge = 1
+    elif numSegRaw < 10:
+        minEdge = 2
+    elif numSegRaw < 40:
+        minEdge = 3
+    elif numSegRaw < 70:
+        minEdge = 4
+    else:
+        minEdge = 5
+
+    return minEdge
+
 cpdef assembleCluster(Cluster[::1] cltView, int startIdx, int taskSize, int numThread):
-    cdef int i, exitCode, endIdx
+    cdef int i, exitCode, endIdx, minEdge
     cdef str cmd, prefix
     cdef object subProcess
 
@@ -18,9 +34,10 @@ cpdef assembleCluster(Cluster[::1] cltView, int startIdx, int taskSize, int numT
         if isSomaClt(&cltView[i]):
             continue
 
+        minEdge = getMinEdge(cltView[i].numSegRaw)
         prefix = "tmp_assm/{}_{}".format(cltView[i].tid, cltView[i].idx)
-        cmd = "wtdbg2 -l 256 -e 1 -S 1 --rescue-low-cov-edges --node-len 256 --ctg-min-length 256 " \
-              "--ctg-min-nodes 1 -q -t {} -i {}.fa -fo {}".format(numThread, prefix, prefix)
+        cmd = "wtdbg2 -l 256 -e {} -S 1 --rescue-low-cov-edges --node-len 256 --ctg-min-length 256 " \
+              "--ctg-min-nodes 1 -q -t {} -i {}.fa -fo {}".format(minEdge, numThread, prefix, prefix)
         subProcess = Popen(cmd, stderr=DEVNULL, shell=True, executable='/bin/bash')
         exitCode = subProcess.wait()
 
